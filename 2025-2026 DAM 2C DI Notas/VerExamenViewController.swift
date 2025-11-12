aaa09d48076e80c4c75f362137e3e035a7126695
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2025-11-12 17:55:28 +0100

    Finalización.

---
+++ 2025-2026 DAM 2C DI Notas/VerExamenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerExamenViewController.swift
+//  2025-2026 DAM 2C DI Notas
+//
+//  Created by Ricardo on 12/11/25.
+//
+
+import UIKit
+
+class VerExamenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 2025-2026 DAM 2C DI Notas/models/ExamenModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExamenModel.swift
+//  2025-2026 DAM 2C DI Notas
+//
+//  Created by Ricardo on 12/11/25.
+//
+
+import Foundation
